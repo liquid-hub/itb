4e1da9e71e4b8ae97ab0dad4b63616100572bc69
VladimirIvanin <ivaninvv@mail.ru>
2020-12-25 14:49:36 +0300

    dynamic_basket

--- project/components/dynamic_basket/dynamic_basket.js
+++ project/components/dynamic_basket/dynamic_basket.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-  EventBus.subscribe("update_items:insales:cart", function(cart) {
+  EventBus.subscribe(['order_ready:insales:cart', 'add_items:insales:cart:light'], function(cart) {
     $(".js-dynamic_basket").html(templateLodashRender(cart, "dynamic_basket"));
     // инициализация items
-    InSalesUI.initAjaxInstance($(".js-dynamic_basket"));
+    InSalesUI.bindUpdateCart($(".js-dynamic_basket [data-cart-form]"));
+  });
+
+  EventBus.subscribe('update_items:insales:cart:light', function (cart) {
+    if (cart.order_lines.length == 0) {
+      $(".js-dynamic_basket").html(templateLodashRender(cart, "dynamic_basket"));
+    }else{
+      $.each(cart.order_lines, function(index, line) {
+        $('[data-item-id="'+line.id+'"]').find('[data-cart-item-quantity-text]').text(line.quantity);
+      });
+    }
   });
 });
